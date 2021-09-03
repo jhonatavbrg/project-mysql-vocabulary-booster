e0d734261f92ff799a3e4f06d45bdbfb622b9ca0
jhonatavbrg <jhonatavbrg@gmail.com>
2021-09-03 07:56:40 -0300

    fix challenge six

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,9 @@
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome Completo',
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Nome completo',
 j.job_title AS 'Cargo',
 jh.start_date AS 'Data de início do cargo',
 d.department_name AS 'Departamento'
 FROM hr.job_history AS jh
-INNER JOIN hr.employees as e
+INNER JOIN hr.employees AS e
 ON jh.employee_id = e.employee_id
 INNER JOIN hr.jobs AS j
 ON jh.job_id = j.job_id
