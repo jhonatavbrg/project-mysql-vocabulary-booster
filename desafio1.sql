4ace46b1e499d8fdfd93b46944a4e8f6eccebf5d
jhonatavbrg <jhonatavbrg@gmail.com>
2021-09-03 04:24:56 -0300

    fix indentation challenges

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,4 @@
-SELECT country_name 
-AS 'País',
-IF (REGION_ID = 1, 'incluído', 'não incluído')
-AS 'Status Inclusão'
+SELECT country_name AS 'País',
+IF (REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY país;
